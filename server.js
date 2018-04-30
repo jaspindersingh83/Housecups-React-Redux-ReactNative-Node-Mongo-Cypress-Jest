1d188128d2584ea8b7dc3b4c835c5d5454e97b20
Kwaku Dapaah <kwakudapaah@Kwakus-MBP.fios-router.home>
2018-04-30 16:03:57 -0400

    implemented mLab for mongo db and removed local server

--- server.js
+++ server.js
@@ -10,6 +10,28 @@ const server = express();
 
 server.use(bodyParser.json());
 
+const port = 5050;
+
+const dbuser = process.env.DB_USER;
+const dbpassword = process.env.DB_PASSWORD;
+
+
+//const dbPath = 'mongodb://localhost/teams';
+const dbPath = `mongodb://${dbuser}:${dbpassword}@ds111050.mlab.com:11050/house_cup_db`;
+
+
+mongoose.Promise = global.Promise;
+
+mongoose
+.connect(dbPath)
+.then(() => {
+    console.log('database is connected sucessfully')
+})
+.catch( (error) => {
+    console.log('Database connection failed')
+});
+
+
 server.get('/', (req, res) => {
     res.status(200).json( {message: 'welcome to the house cup API'} );
 })
@@ -29,29 +51,8 @@ server.post('/api/person', (req, res) => {
     })
 })
 
-const port = 5050;
-
-const dbuser = process.env.DB_USER;
-const dbpassword = process.env.DB_PASSWORD;
 
 
-//const dbPath = 'mongodb://localhost/teams';
-const dbPath = `mongodb://${dbuser}:${dbpassword}@ds111050.mlab.com:11050/house_cup_db`;
-
-
-mongoose.Promise = global.Promise;
-
-mongoose
-.connect(dbPath)
-.then(()=> {
-    server.listen(port, () => {
-        console.log(`server is running on ${port}`);
-})
-
-})
-.catch( (error) => {
-    console.log('Database connection failed')
-});
 
 
 

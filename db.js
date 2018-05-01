e4b46d760b8b83a0af3760f5ecc8840633c41978
William Antony <william@pathadan.com>
2018-04-30 19:35:33 -0500

    Doing commit to test CI Build

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 // Database Information
 const dbuser = process.env.DB_USER;
 const dbpassword = process.env.DB_PASSWORD;
 const dbPath = `mongodb://${dbuser}:${dbpassword}@ds111050.mlab.com:11050/house_cup_db`;
 
-mongoose.Promise = global.Promise;
-
-mongoose
-  .connect(dbPath)
+mongoose.connect(dbPath)
   .then(() => {
     console.log('database is connected sucessfully');
   })

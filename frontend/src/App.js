52373fc1e7425f60c0f3edd40af54cfa05d25847
William Antony <william@pathadan.com>
2018-05-02 23:36:05 -0500

    Added Routes for settings & forgotpassword

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,9 @@ import Header from './components/Header/Header';
 // Views for Authentication
 import Signin from './components/Auth/Signin';
 import Signup from './components/Auth/Signup';
+import Settings from './components/Auth/Settings';
 import forgotpassword from './components/Auth/forgotpassword';
+import reset from './components/Auth/reset';
 // General View Pages
 import Landing from './components/Landing/Landing';
 import Pricing from './components/Pricing/Pricing';
@@ -32,7 +34,9 @@ class App extends Component {
               <Route exact path="/pricing" component={Pricing} />
               <Route exact path="/signin" component={Signin} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/settings" component={Settings} />
               <Route exact path="/forgotPassword" component={forgotpassword} />
+              <Route exact path="/reset" component={reset} />
             </Switch>
           </div>
         </Router>

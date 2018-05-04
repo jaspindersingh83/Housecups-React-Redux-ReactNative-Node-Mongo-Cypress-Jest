d6889992fa6e81f335e53471656a0fde889a69c4
William Antony <william@pathadan.com>
2018-05-03 22:30:02 -0500

    Added PrivateRoute and PublicAuthRoute to appropriate routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,8 @@ import reset from './components/Auth/reset';
 import Landing from './components/Landing/Landing';
 import Pricing from './components/Pricing/Pricing';
 import Dashboard from './components/Dashboard/Dashboard';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import PublicAuthRoute from './components/PublicAuthRoute/PublicAuthRoute';
 
 const ReduxStore = createStore(Reducers, applyMiddleware(ReduxPromise));
 
@@ -31,12 +33,13 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={Landing} />
               <Route exact path="/pricing" component={Pricing} />
-              <Route exact path="/signin" component={Signin} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/forgotPassword" component={forgotpassword} />
-              <Route exact path="/reset" component={reset} />
               
-              <Route exact path="/(dashboard|schools|houses|scoreboard|settings)" component={Dashboard} />
+              <PublicAuthRoute exact path="/signin" component={Signin} />
+              <PublicAuthRoute exact path="/signup" component={Signup} />
+              <PublicAuthRoute exact path="/forgotPassword" component={forgotpassword} />
+
+              <PrivateRoute exact path="/reset" component={reset} />              
+              <PrivateRoute exact path="/(dashboard|schools|houses|scoreboard|settings)" component={Dashboard} />
             </Switch>
           </div>
         </Router>

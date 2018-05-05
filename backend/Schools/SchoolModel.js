4b9ece2a682631bd7ff902817a884418bb65ce26
Kwaku Dapaah <kwakudapaah@Kwakus-MBP.fios-router.home>
2018-05-05 14:08:17 -0400

    added school model

---
+++ backend/Schools/SchoolModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const SchoolSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  admin: {
+    type: String,
+    required: true,
+  },
+  teachers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }], // an array of object ids that will come from the user model.
+  houses: [{
+    type: Schema.Types.ObjectId,
+    ref: 'House',
+  }], // an array of ids that will come from house model.
+  location: String,
+  createdOn: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('School', SchoolSchema);

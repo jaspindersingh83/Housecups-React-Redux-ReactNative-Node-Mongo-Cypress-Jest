0be33e38becfdf4cd8b4f1e7a1a986f1a1ceb7fa
William Antony <william@pathadan.com>
2018-05-08 00:57:19 -0500

    Created getFontColor() to return font color based on background color

--- frontend/src/components/Scoreboard/ScoreCard.js
+++ frontend/src/components/Scoreboard/ScoreCard.js
@@ -33,15 +33,44 @@ class ScoreCard extends Component {
     });
     // Error Handling
     Socket.on('error', (data) => {
-      console.log(data);
       this.props.history.push('/signin');
     });
+  }
+
+  getFontColor = (backgroundColor) => {
+    // Returns Font-color
+    // based on the luminance of Background-color
+    if (new RegExp(/[^a-f0-9#]/gi).test(backgroundColor)) {
+      // invalid Hex
+      return 'rgb(0, 0, 0)';
+    }
+    const hexColor = backgroundColor.replace(/#/gi, '');
+    // values constant that determines the luminance
+    const factor = {
+      R: 0.299,
+      G: 0.587,
+      B: 0.114,
+    };
+    const midPoint = 100;
+    // Convert Hex into rgb array
+    const rgb = Array.from(hexColor).reduce((rgbArray, hexDigit, index, hexDigitsArray) => {
+      if (index % 2 === 0) {
+        const hexValue = hexDigit + hexDigitsArray[index + 1];
+        rgbArray.push(parseInt(hexValue, 16));
+      }
+      return rgbArray;
+    }, []);
+
+    const luminance = (rgb[0] * factor.R) + (rgb[1] * factor.G) + (rgb[2] * factor.B);
+
+    return (luminance >= midPoint) ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)';
   }
 
   render() {
     const { house } = this.props;
     const styles = {
       ScoreCard: {
+        color: this.getFontColor(house.color),
         backgroundColor: house.color,
       },
     };
@@ -57,12 +86,12 @@ class ScoreCard extends Component {
             className="ScoreCard__button ScoreCard__button--decrement"
             onClick={() => this.decreaseScore()}
           />
+          <div className="ScoreCard__score">{house.score}</div>
           <button
             className="ScoreCard__button ScoreCard__button--increment"
             onClick={() => this.increaseScore()}
           />
         </div>
-        <div className="ScoreCard__score">{house.score}</div>
       </div>
     );
   }

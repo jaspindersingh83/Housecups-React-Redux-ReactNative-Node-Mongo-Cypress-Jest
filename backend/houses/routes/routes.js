8f07b2c1300ced63868455062c9a5c5e1f2eac59
JASPINDER SING <jaspindersingh83@gmail.com>
2018-05-10 00:06:59 -0700

    Houses and Schools Complete on backend

--- backend/houses/routes/routes.js
+++ backend/houses/routes/routes.js
@@ -3,7 +3,6 @@ const {
   deleteHouse,
   updateHouse,
   getHouseBySchool,
-  getHouseById,
 } = require('../controllers/HouseController');
 
 const { authenticate } = require('../../common/common');
@@ -11,7 +10,6 @@ const { authenticate } = require('../../common/common');
 module.exports = (server) => {
   server.route('/api/house').post(authenticate, addHouse);
   server.route('/api/house').get(authenticate, getHouseBySchool);
-  server.route('/api/house/:id').get(authenticate, getHouseById);
   server.route('/api/house/:id').delete(authenticate, deleteHouse);
   server.route('/api/house/:id').put(authenticate, updateHouse);
 };

461378a9c153e5c1e4066b16bf315394689360d7
William Antony <william@pathadan.com>
2018-05-12 10:54:09 -0500

    Added/modified links in Sidebar

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -12,14 +12,23 @@ class Sidebar extends Component {
 
           <nav className="Sidebar__navigation">
             <ul>
+              <Link to="/dashboard">
+                <li data-selected={ pathname === '/dashboard' }>Summary</li>
+              </Link>
               <Link to="/schools">
-                <li data-selected={ pathname === '/schools' }>Schools</li>
+                <li data-selected={ pathname === '/schools' }>Create School</li>
+              </Link>
+              <Link to="/houses">
+                <li data-selected={ pathname === '/houses' }>Manage Houses</li>
+              </Link>
+              <Link to="/teachers">
+                <li data-selected={ pathname === '/teachers' }>Manage Teachers</li>
               </Link>
               <Link to="/scoreboard">
                 <li data-selected={ pathname === '/scoreboard' }>Score Board</li>
               </Link>
               <Link to="/settings">
-                <li data-selected={ pathname === '/settings' }>Settings</li>
+                <li data-selected={ pathname === '/settings' }>User Settings</li>
               </Link>
             </ul>
           </nav>

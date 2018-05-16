15330b50805c75243d1179cbdc61a30fc263f949
William Antony <william@pathadan.com>
2018-05-15 22:35:51 -0500

    Implemented UserRoles for routing in AccountOverview

--- frontend/src/components/AccountOverview/AccountOverview.js
+++ frontend/src/components/AccountOverview/AccountOverview.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getHousesBySchool, getTeachers } from '../../actions'; 
+import { getUserRoles } from '../../actions';
 import './AccountOverview.css';
+import SchoolInfoOverview from './components/SchoolInfoOverview/SchoolInfoOverview';
 
 class AccountOverview extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      houses: [],
-      teachers: [],
+      auth: {},
     };
-    console.warn('AccountOverview ::', 'Get the User\'s name & School name and update in the render()');
   }
 
-  async componentDidMount() {
-    await this.props.getHousesBySchool(this.props.history);
-    await this.props.getTeachers(this.props.history);
+  async componentWillMount() {
+    await this.props.getUserRoles(this.props.history);
   }
 
   componentWillReceiveProps(props) {
+    const { isSuperAdmin, isSchoolAdmin, isTeacher } = props.auth;
+    if (!isSchoolAdmin && !isSchoolAdmin && !isTeacher) {
+      this.props.history.push('/schools');
+    }
     this.setState({
-      houses: [ ...props.houses ],
-      teachers: [ ...props.teachers ],
+      auth: { ...props.auth },
     });
   }
 
@@ -34,27 +34,9 @@ class AccountOverview extends Component {
           <div className="User__name">John Doe</div>
           <div className="User__role">Admin</div>
         </div>
-        <div className="School">
-          <div className="School__name">Lambda School</div>
-          <div className="School__info">
-            <div className="School__info__text">
-              Total number of Houses:
-              <span className="number">{ this.state.houses.length }</span>
-            </div>
-            <div className="School__info__action">
-              <Link to="/houses">See all Houses</Link>
-            </div>
-          </div>
-          <div className="School__info">
-            <div className="School__info__text">
-              Total number of Teachers:
-              <span className="number">{ this.state.teachers.length }</span>
-            </div>
-            <div className="School__info__action">
-              <Link to="/teachers">See all Teachers</Link>
-            </div>
-          </div>
-        </div>
+        {
+          (this.state.auth.isAdmin) ? <SchoolInfoOverview /> : null
+        }
       </div>
     );
   }
@@ -65,4 +47,8 @@ const mapStateToProps = (state) => {
   return state;
 };
 
-export default connect(mapStateToProps, { getHousesBySchool, getTeachers })(AccountOverview);
+const mapDispatchToProps = {
+  getUserRoles,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountOverview);
